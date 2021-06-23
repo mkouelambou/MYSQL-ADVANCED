dae3ec6d55e632cf85e38dd049feb290d8adec66
Mike <mike.kouelambou@student.rocva.nl>
2021-06-23 14:43:00 +0200

    save code

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,13 @@
 -- Opdracht 1
-
+SELECT * FROM studenten2 WHERE woonplaats = "Amstelveen"
 -- Opdracht 2 
-
+UPDATE studenten2 SET adres = "Elandsgracht 9", postcode = "1173EC" WHERE student_id = 8
+UPDATE studenten2 SET adres = "Spiegelgracht 16", postcode = "1097AD" WHERE student_id = 9
+UPDATE studenten2 SET adres = "Amstelveenseweg 37", postcode = "1457BG" WHERE student_id = 10
 -- Opdracht 3
-
+UPDATE `studenten2` SET `geboortedatum` = '2000-12-12' WHERE `studenten2`.`student_id` = 2;
 -- Opdracht 4
-
+UPDATE `studenten2` SET klas = '9C' WHERE student_id = 4;
+UPDATE `studenten2` SET klas = '9A' WHERE student_id = 8;
 -- Opdracht 5
+UPDATE `studenten2` SET voornaam = 'Johannah' WHERE student_id = 6;
--- 2-UPDATE/taak02 - Update 2/antwoorden.sql
+++ 2-UPDATE/taak02 - Update 2/antwoorden.sql
@@ -1,7 +1,7 @@
 -- Opdracht 1
-
+UPDATE studenten2 SET klas = "9X" WHERE klas = "9A"
 -- Opdracht 2 
-
+UPDATE studenten2 SET klas = "9Z" WHERE student_id > 7
 -- Opdracht 3
 
 -- Opdracht 4
