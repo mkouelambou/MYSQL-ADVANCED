e83254a50fef485ceaf0ae168af47449879df782
Mike <mike.kouelambou@student.rocva.nl>
2021-06-23 14:01:35 +0200

    save code

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,9 @@
+INSERT INTO studenten (voornaam,achternaam,klas,geboortedatum,woonplaats)
+VALUES ('Max', 'Van Bergen', '9B', '2003-03-28', 'Amsterdam')
 
+
+INSERT INTO studenten (voornaam,achternaam,klas,geboortedatum,woonplaats)
+VALUES ('Alex', 'Stuard', '9D', '2000-12-06', 'Amstelveen')
+
+INSERT INTO studenten (voornaam,achternaam,klas,geboortedatum,woonplaats)
+VALUES ('Betty', 'Van Duin', '9A', '2002-01-19', 'Utrecht')
